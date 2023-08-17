b0e82d18d56297424207b54ffa4fda51ca5f69db
tony <agnstege@gmail.com>
2023-08-17 14:15:54 +0000

    Cert Allowed

--- kc.bat
+++ kc.bat
@@ -1,5 +1,5 @@
 REM
 REM kc.py KMIP Key Transfer batch file with values
 REM
-py kc.py -srcHost 192.168.50.100 -srcUser mary_kmip_test -srcPass Thales123! -dstHost 192.168.50.200 -dstUser tony_kmip_test -dstPass Thales123! -srcclientCert cert100.pem -srcclientKey key100.pem -srcCAs ca-100.txt -dstclientCert cert200.pem -dstclientKey key200.pem -dstCAs ca-200.txt 
+py kc.py -srcHost 192.168.50.100 -dstHost 192.168.50.200 -dstUser tony_kmip_test -dstPass Thales123! -srcclientCert cert100.pem -srcclientKey key100.pem -srcCAs ca-100.txt -dstclientCert cert200.pem -dstclientKey key200.pem -dstCAs ca-200.txt 
 
